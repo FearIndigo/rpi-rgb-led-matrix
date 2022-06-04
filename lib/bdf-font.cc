c561af7a1255403cb10ff68d43570d2d619bc83b
FearIndigo <josephm@fearindigo.com.au>
2022-06-04 12:18:22 +0800

    -fix offset

--- lib/bdf-font.cc
+++ lib/bdf-font.cc
@@ -211,9 +211,11 @@ int Font::DrawGlyphInverted(Canvas *c, int x_pos, int y_pos,
     const rowbitmap_t& row = g->bitmap[y];
     for (int x = 0; x < g->device_width; ++x) {
       if (row.test(kMaxFontWidth - 1 - x)) {
-        c->SetPixel(x_pos + (g->device_width - x), y_pos + (g->height - y), color.r, color.g, color.b);
+        c->SetPixel(x_pos + (g->device_width - x) - g->device_width / 2, y_pos + (g->height - y),
+          color.r, color.g, color.b);
       } else if (bgcolor) {
-        c->SetPixel(x_pos + (g->device_width - x), y_pos + (g->height - y), bgcolor->r, bgcolor->g, bgcolor->b);
+        c->SetPixel(x_pos + (g->device_width - x) - g->device_width / 2, y_pos + (g->height - y),
+          bgcolor->r, bgcolor->g, bgcolor->b);
       }
     }
   }
